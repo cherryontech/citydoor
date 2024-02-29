9b4185d179d6cf83ad23201b73001e02410cf916
Laura Laqui <lauralaqui@gmail.com>
2024-02-29 13:19:38 -0500

    chore: add SB story for heading

---
+++ src/stories/Heading.stories.js
@@ -0,0 +1,23 @@
+import Heading from '../app/components/heading';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Heading',
+  component: Heading,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Base = {
+  args: {
+    className: "test",
+    children: 'coffee',
+    level: 4
+  },
+};
+
