9d7466221dd64cccee47eee714d914ad2ad715ee
novellac <38117965+novellac@users.noreply.github.com>
2024-04-02 18:33:28 -0400

    chore: Allow SB to use compiled TW

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,9 @@
+import '../src/app/globals.css'
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
@@ -9,6 +11,6 @@ const preview = {
       },
     },
   },
-};
+}
 
-export default preview;
+export default preview
