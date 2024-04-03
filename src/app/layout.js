d82d0bba63c8f61ff343f8d1a30fe79917f6431e
novellac <38117965+novellac@users.noreply.github.com>
2024-04-03 09:16:58 -0400

    feat: Add logo image and alt to header Added in the layout for repeated use

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
+import cdLogoHorizontal from '/public/cdLogoHorizontal.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,8 +11,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        
+        <header>
+          <Image
+            className="mx-auto"
+            height={64}
+            src={cdLogoHorizontal}
+            alt="CityDoor logo, an abstract line drawing of a cityscape with several buildings and the words City Door & Hardware below it."
+          />
+        </header>
+
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
