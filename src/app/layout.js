358a027f917abc80fff7e9736837664f539edee9
novellac <38117965+novellac@users.noreply.github.com>
2024-04-12 18:50:15 -0400

    feat: Add contact info and style header

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Image from 'next/image'
 import cdLogoHorizontal from '/public/cdLogoHorizontal.png'
+import IconPhone from './components/icon-phone'
+import IconEnvelope from './components/icon-envelope'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +15,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={`${inter.className} p-4 cd-desktop:p-0`}>
-        <header>
+        <header className='mb-10 cd-desktop:mb-8'>
+          <div className='flex flex-col items-center gap-2 px-4 py-4 mb-10 cd-desktop:gap-6 cd-desktop:px-20 cd-desktop:flex-row bg-cd-tertiary cd-desktop:py-5 cd-desktop:mb-6'>
+            <div className='flex items-center gap-1'>
+              <IconPhone className='text-cd-black' /> <p>(+1) 415-431-0400</p>
+            </div>
+            <div className='flex items-center gap-1'>
+              <IconEnvelope className='text-cd-black' />
+              <a className='underline' href='mailto:sales@citydoor.com'>
+                sales@citydoor.com
+              </a>
+            </div>
+          </div>
           <Image
             className='mx-auto'
             height={64}
