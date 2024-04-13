a5b202d66f9a0f5f34becb52c3d1741dd63a0d3c
novellac <38117965+novellac@users.noreply.github.com>
2024-04-13 12:01:15 -0400

    feat: Add homepage content

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en' className={`${roboto.variable}`}>
       <body className='flex flex-col items-stretch h-screen p-4 cd-desktop:p-0'>
-        <header className='mb-10 cd-desktop:mb-8'>
+        <header>
           <div className='flex flex-col items-center gap-2 px-4 py-4 mb-10 cd-desktop:gap-6 cd-desktop:px-20 cd-desktop:flex-row bg-cd-tertiary cd-desktop:py-5 cd-desktop:mb-6'>
             <div className='flex items-center gap-1'>
               <IconPhone className='text-cd-black' /> <p>(+1) 415-431-0400</p>
@@ -41,12 +41,12 @@ export default function RootLayout({ children }) {
           />
         </header>
 
-        <main className='grow'>{children}</main>
+        <main className='grow cd-desktop:px-20'>{children}</main>
 
         <footer className='px-3 pt-5 cd-desktop:pt-6 cd-desktop:px-0 bg-cd-accent'>
-          <div className='flex flex-col justify-start gap-5 mb-6 cd-desktop:mb-11 cd-desktop:gap-20 cd-desktop:flex-row'>
+          <div className='flex flex-col justify-start gap-5 mb-6 cd-desktop:px-20 cd-desktop:mb-11 cd-desktop:gap-20 cd-desktop:flex-row'>
             <Image
-              className='h-12 cd-desktop:h-[88px] object-contain'
+              className='h-12 cd-desktop:h-[88px] cd-desktop:w-[88px] mx-auto cd-desktop:mx-0 object-contain'
               src={cdLogoSquare}
               alt='CityDoor logo, the words City Door & Hardware partially surrounded by a thin red square border.'
             />
@@ -70,7 +70,7 @@ export default function RootLayout({ children }) {
             </address>
           </div>
 
-          <div className='px-3 pb-6 text-center border-t border-cd-black pt-9 cd-desktop:py-3'>
+          <div className='px-3 pb-6 text-center border-t cd-desktop:pl-20 cd-desktop:text-left border-cd-black pt-9 cd-desktop:py-3'>
             <p className='text-sm'>© 2024 City Door. All rights reserved.</p>
           </div>
         </footer>
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,38 @@
-import Nav from './nav'
 import Heading from './components/heading'
-import ImageCard from './components/image-card'
+import cdBannerMain from '/public/cdBannerMain.png'
+import cdBannerMainDesktop from '/public/cdBannerMain-desktop.png'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <Heading level={1} className={"text-cd-brand text-4xl lg:text-6xl"}>Leading Door and Door Hardware Experts</Heading>
-      <ol>
-        <ImageCard 
-          alt="Sweet little baby white dog being held up by clapping hands."
-          descriptionText="I'm baby praxis artisan edison bulb, church-key master cleanse tumeric tofu chambray aesthetic food truck. Lo-fi tumblr selvage williamsburg gentrify cloud bread authentic polaroid green juice small batch tbh celiac roof party raw denim hashtag. Mlkshk distillery vegan helvetica, try-hard authentic lyft copper mug polaroid church-key tumeric yr iceland listicle fit. Sartorial palo santo austin woke. Hoodie crucifix meditation, pitchfork whatever gochujang freegan schlitz."
-          headingText="Things that fit in one's purse"
-          src="https://i.redd.it/gm1zqjjcm8351.jpg" 
-          Tag={'li'}/>
-      </ol>
-    </div>
+    <article className='my-[90px] cd-desktop:flex gap-44 justify-between items-center'>
+      <Image
+        className='mx-auto mb-6 cd-desktop:hidden'
+        width={320}
+        src={cdBannerMain}
+        alt='Partial view of double glass doors and a tall window. The doors and window are each framed in white wood with cascading ivy. A small decorative plaque hanging from the door reads Home.'
+      />
+      <div className='max-w-[320px] cd-desktop:max-w-[613px] mx-auto cd-desktop:mx-0'>
+        <Heading
+          level={1}
+          className={
+            'text-cd-brand cd-desktop:text-6xl text-4xl mb-6 cd-desktop:mb-4'
+          }
+        >
+          Leading Door and Door Hardware Experts
+        </Heading>
+        <p className='text-xl cd-desktop:text-2xl'>
+          CityDoor & Hardware has been the authority on commercial and
+          residential doors, ADA door operators, and door hardware in the Bay
+          Area since 1992. We distribute high-quality products worldwide.
+        </p>
+      </div>
+      <Image
+        className='hidden cd-desktop:block'
+        height={536}
+        src={cdBannerMainDesktop}
+        alt='Partial view of double glass doors and a tall window. The doors and window are each framed in white wood with cascading ivy. A small decorative plaque hanging from the door reads Home.'
+      />
+    </article>
   )
 }
